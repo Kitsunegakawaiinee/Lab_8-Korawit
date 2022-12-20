27ae93b9cb53235a485535a854cf132d8a56573d
SKOT <Korawit_pi@cmu.ac.th>
2022-12-20 16:38:33 +0700

    ข้อ 2

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -16,3 +16,54 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name,Movie,Day,Say;
+    int gear;
+    //char a1,a2;
+    cout <<"Fahsai: Sawadee ka...Can you tell me your name?" << endl << "?????: ";
+    getline(cin,name);
+    cout << "Fahsai: Wow!!! "<< name << " is a really cool name." << endl;
+    name = name+": ";
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?" << endl << name;
+    cin >> gear;
+    cin.ignore();
+    gear = (gear/10000000) -12;
+    cout << "Fahsai: I think you may be GEAR " << gear<<". I have a free movie ticket for you." << endl << "Fahsai: Let's go to the cinema together!!!" << endl << "Fahsai: What movie do you want to watch?" << endl << name;
+    getline(cin,Movie);
+    cout << "Fahsai: So....which day are you free to go with me?" << endl<<name;
+    getline(cin,Day);
+    cout << "Fahsai: " << Day <<"....that is OK!!! I'm looking forward to watching " << Movie <<" with you." << endl << name;
+    getline (cin,Say);
+    cout << "Fahsai: 555+ see you "<< Day <<". Bye Bye \\(^ ^)/";
+
+
+
+
+    
+
+
+
+
+    // a1 = gear[0];
+    // a2 = gear[1];
+    // cout << a1 << " " << a2 << endl;
+    // gear1 += a1;
+    // gear1 += a2;
+    // resultgear = int (gear1);
+
+    // cout << resultgear;
+
+
+
+
+
+
+    
+
+}
